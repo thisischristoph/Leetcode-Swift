f02d33bd0c912ef99c207701d01ec9243e2e8f87
Chris <christopher.harrison.2011@gmail.com>
2021-08-08 15:43:49 +0100

    509. Fibonacci Number

--- Dynamic Programming/Sources/Dynamic Programming/Easy/509. Fibonacci Number.playground/Contents.swift
+++ Dynamic Programming/Sources/Dynamic Programming/Easy/509. Fibonacci Number.playground/Contents.swift
@@ -2,9 +2,28 @@ import UIKit
 
 func fib(_ n: Int) -> Int {
     
-    for 
-        
-    return 10
+    if n == 0 { return 0 }
+    
+    var num0 = 0
+    var num1 = 1
+    
+    for num in 0..<n {
+        if (num % 2) == 0 {
+            num1 = num0 + num1
+        } else {
+            num0 = num0 + num1
+        }
+    }
+    
+    return n % 2 == 0 ? num0: num1
 }
+
+fib(0)
+fib(1)
+fib(2)
+fib(3)
+fib(4)
+fib(5)
+fib(6)
 
 
